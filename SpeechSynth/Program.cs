4bdba267154a41387ee5f4b9c48120b14b5761ac
mred5683 <hansenrd5683@gmail.com>
2016-12-05 02:47:20 -0700

    commented out example code and created instance of SpeechSynthesizer to take user input and speak what is typed in the console.

--- SpeechSynth/Program.cs
+++ SpeechSynth/Program.cs
@@ -1,58 +1,69 @@
 ﻿using System;
+using System.Data;
+using static System.Console;
 using System.Speech.Synthesis;
-using System.Speech.AudioFormat;
 
-namespace SampleSynthesis
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace SpeechSynth {
 
-            // Initialize a new instance of the SpeechSynthesizer.
-            using (SpeechSynthesizer synth = new SpeechSynthesizer())
-            {
+  class Program {
+    static void Main(string[] args) {
+      #region
 
-                // Output information about all of the installed voices. 
-                Console.WriteLine("Installed voices -");
-                foreach (InstalledVoice voice in synth.GetInstalledVoices())
-                {
-                    VoiceInfo info = voice.VoiceInfo;
-                    string AudioFormats = "";
-                    foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
-                    {
-                        AudioFormats += String.Format("{0}\n",
-                        fmt.EncodingFormat.ToString());
-                    }
+      ////trying to understand the SpeechSynthesizer Class and methods
+      
 
-                    Console.WriteLine(" Name:          " + info.Name);
-                    Console.WriteLine(" Culture:       " + info.Culture);
-                    Console.WriteLine(" Age:           " + info.Age);
-                    Console.WriteLine(" Gender:        " + info.Gender);
-                    Console.WriteLine(" Description:   " + info.Description);
-                    Console.WriteLine(" ID:            " + info.Id);
-                    Console.WriteLine(" Enabled:       " + voice.Enabled);
-                    if (info.SupportedAudioFormats.Count != 0)
-                    {
-                        Console.WriteLine(" Audio formats: " + AudioFormats);
-                    }
-                    else
-                    {
-                        Console.WriteLine(" No supported audio formats found");
-                    }
+      var userInput = string.Empty;
+      var synth = new SpeechSynthesizer();
+      while (userInput != "exit") {
+        WriteLine("what would you like me to say?      ---'exit' to quit---");
+        WriteLine("");
+        userInput = ReadLine();
 
-                    string AdditionalInfo = "";
-                    foreach (string key in info.AdditionalInfo.Keys)
-                    {
-                        AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
-                    }
+        synth.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+        synth.Speak(userInput);
 
-                    Console.WriteLine(" Additional Info - " + AdditionalInfo);
-                    Console.WriteLine();
-                }
-            }
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
+      }
+
+      #endregion
+
+      //example code pulled down from tutorial
+
+      #region
+
+      //// Initialize a new instance of the SpeechSynthesizer.
+      //using (var synth = new SpeechSynthesizer()) {
+
+      //  // Output information about all of the installed voices. 
+      //  Console.WriteLine("Installed voices -");
+      //  foreach (var voice in synth.GetInstalledVoices()) {
+      //    var info = voice.VoiceInfo;
+      //    var AudioFormats = "";
+      //    foreach (var fmt in info.SupportedAudioFormats)
+      //      AudioFormats += string.Format("{0}\n",
+      //        fmt.EncodingFormat);
+
+      //    Console.WriteLine(" Name:          " + info.Name);
+      //    Console.WriteLine(" Culture:       " + info.Culture);
+      //    Console.WriteLine(" Age:           " + info.Age);
+      //    Console.WriteLine(" Gender:        " + info.Gender);
+      //    Console.WriteLine(" Description:   " + info.Description);
+      //    Console.WriteLine(" ID:            " + info.Id);
+      //    Console.WriteLine(" Enabled:       " + voice.Enabled);
+      //    if (info.SupportedAudioFormats.Count != 0) Console.WriteLine(" Audio formats: " + AudioFormats);
+      //    else Console.WriteLine(" No supported audio formats found");
+
+      //    var AdditionalInfo = "";
+      //    foreach (var key in info.AdditionalInfo.Keys) AdditionalInfo += string.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
+
+      //    Console.WriteLine(" Additional Info - " + AdditionalInfo);
+      //    Console.WriteLine();
+      //  }
+      //}
+
+      //Console.WriteLine("Press any key to exit...");
+      //Console.ReadKey();
+
+      #endregion
     }
+  }
 }
